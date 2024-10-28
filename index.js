5de501c6265d8684c59aaeb46c9165fd2d665ce5
bartschliam <bartschliam@gmail.com>
2024-10-28 06:09:22 +0100

    updated small changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const { JWT } = require('google-auth-library');
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { JWT } = require("google-auth-library");
 
 const client = new Client({
   intents: [
@@ -15,14 +15,14 @@ const CHANNEL_ID = process.env.CHANNEL_ID;
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 const REGEX_PATTERN = new RegExp(process.env.REGEX_PATTERN);
 const SCOPES = [
-  'https://www.googleapis.com/auth/spreadsheets',
-  'https://www.googleapis.com/auth/drive.file',
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
 ];
 const base64EncodedServiceAccount = process.env.BASE64_ENCODED_SERVICE_ACCOUNT;
 const decodedServiceAccount = Buffer.from(
   base64EncodedServiceAccount,
-  'base64'
-).toString('utf-8');
+  "base64"
+).toString("utf-8");
 const credentials = JSON.parse(decodedServiceAccount);
 const serviceAccountAuth = new JWT({
   email: credentials.client_email,
@@ -36,6 +36,7 @@ async function addToSheet(message) {
   await doc.loadInfo(); // loads document properties and worksheets
   console.log(doc.title);
   const sheet = doc.sheetsByIndex[0];
+  message.content = message.content.replace(/\?question\s*/, "").trim();
   console.log(message.content);
   await sheet.addRow({
     Timestamp: message.createdAt.toISOString(),
@@ -44,20 +45,22 @@ async function addToSheet(message) {
   });
 }
 
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
   if (
     message.channel.id === CHANNEL_ID &&
     REGEX_PATTERN.test(message.content)
   ) {
     try {
       await addToSheet(message);
-      console.log('Message added to sheet successfully');
+      console.log("Message added to sheet successfully");
+      await message.react("✅"); // Use any emoji you prefer
     } catch (error) {
-      console.error('Error adding message to sheet:', error);
+      console.error("Error adding message to sheet:", error);
+      await message.react("❌"); // React with a different emoji to indicate failure
     }
   }
 });
